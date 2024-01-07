451759c3fc1f36195e5d41fa5234e56e627ed830
NguyenKien <dacnguyen732003@gmail.com>
2024-01-07 21:48:59 +0700

    update1

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let nav = document.querySelector(".header__bottom");
+let content = document.querySelector(".content");
+const navOffSet = nav.offsetTop;
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= navOffSet) {
+    nav.classList.add("sticky");
+    content.style.marginTop = "80px";
+  } else {
+    nav.classList.remove("sticky");
+    content.style.marginTop = "0";
+  }
+});
+
+// Navbar mobile
+let bar = document.querySelector(".fa-bars");
+let listBar = document.querySelector(
+  ".header .header__bottom .container .box:nth-child(3) ul"
+);
+let closeBar = document.querySelector(".fa-xmark");
+bar.addEventListener("click", () => {
+  listBar.classList.add("open-bar");
+});
+closeBar.addEventListener("click", function () {
+  listBar.classList.remove("open-bar");
+});
